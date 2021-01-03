c78297c5108643f9e0cf11855a2de722a86da994
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-03 19:30:22 +0700

    Implements asynchronous task using kotlin coroutines

--- app/src/main/java/io/erikrios/github/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/mybackgroundthread/MainActivity.kt
@@ -1,12 +1,14 @@
 package io.erikrios.github.mybackgroundthread
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
-import java.util.concurrent.Executors
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +18,16 @@ class MainActivity : AppCompatActivity() {
         val btnStart: Button = findViewById(R.id.btn_start)
         val tvStatus: TextView = findViewById(R.id.tv_status)
 
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
+
 
         btnStart.setOnClickListener {
-            executor.execute {
+            lifecycleScope.launch(Dispatchers.Default) {
                 try {
                     // Simulate process compressing
                     for (i in 0..10) {
-                        Thread.sleep(500)
+                        delay(500)
                         val percentage = i * 10
-                        handler.post {
+                        withContext(Dispatchers.Main) {
                             // Update ui in main thread
                             if (percentage == 100) {
                                 tvStatus.text = getString(R.string.task_completed)
